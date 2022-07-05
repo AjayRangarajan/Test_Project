8edb90beba317cdedc72838c31ee2db73ef7373f
AjayRangarajan <ajayrangarajan5@gmail.com>
2022-07-05 11:03:29 +0530

    Print MSG variable in test_model

--- home/unittests/test_model.py
+++ home/unittests/test_model.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-
+import os
 class UserTestCase(TestCase):
 	def setUp(self):
 		User.objects.create_user(username="test_user", password="test_user_password")
 
 	def test_user_created(self):
+		print(os.environ.get('MSG'))
 		test_user = User.objects.get(username="test_user")
 		self.assertEqual(test_user.username, 'test_user')
